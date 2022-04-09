9c1ad6bb759c4a40ec7c0ccb082d87a89a15878b
Nikita Kosenko <foxidalim@gmail.com>
2022-04-09 14:01:58 +0300

    Solving the 5.12 task.

---
+++ tasks_11-20/task5-12.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+echo "Input the name of the file:"
+read name
+
+if test -e "$name";
+then 
+    echo "В данном каталоге существует файл с именем $name";
+else
+    echo "В данном каталоге не существует файл с именем $name";
+fi
