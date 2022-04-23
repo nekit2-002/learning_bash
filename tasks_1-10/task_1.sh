08b51816101f13d931266b5e5cef274c771f0ca2
Nikita Kosenko <foxidalim@gmail.com>
2022-04-23 13:34:15 +0300

    Solve task 6.1

---
+++ tasks_1-10/task_1.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+# To run the script on the background 
+# type ./task_1.sh &
+
+#  To stop the program run 
+#  kill PID
+
+n=0
+while true; do
+    echo This is the $n th iteration.
+    n=$(($n + 1))
+    echo 
+    echo -e "The PID of the process is $$ \n"
+    sleep 1
+done
